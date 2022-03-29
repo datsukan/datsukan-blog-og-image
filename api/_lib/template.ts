319fd0edb908e127fae34f0c4ec6204e6e0dbac4
datsukan <s.datsukan@gmail.com>
2022-03-30 04:36:39 +0900

    :art: fix 絵文字の取得処理を修正

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -131,8 +131,8 @@ export function getHtml(parsedReq: ParsedRequest) {
 
 function twemojiURL(emoji: string) {
     const codePoint = twemoji.convert.toCodePoint(emoji)
-    const baseUrl = "https://twemoji.maxcdn.com/v/latest/72x72/"
-    const url = `${baseUrl}${codePoint.split("-")[0]}.png`
+    const baseUrl = "https://twemoji.maxcdn.com/v/latest/svg/"
+    const url = `${baseUrl}${codePoint}.svg`
 
     return url
 }
